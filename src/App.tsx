b3cba8ccd8fe6296f88c1f553614ffc0094fcf21
Tomas Hastrup <thastrup@gmail.com>
2024-10-08 09:40:10 -0300

    goodone

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import Tasks from "./components/tasks/Tasks";
+import Timer from "./components/timer/Timer";
+import TopMenu from "./components/timer/TopMenu";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className=" bg-rose-700 w-screen  flex flex-col justify-center items-center">
+      <div className="flex flex-col items-center  w-screen mt-20 md:w-screen lg:w-[60%]">
+        <TopMenu />
+
+        <div>
+          {" "}
+          <Timer />
+          <Tasks />
+        </div>
+      </div>{" "}
+    </div>
+  );
 }
 
-export default App
+export default App;
