4f68194f660ebd51ecf166a615d5c1dabdeebac4
Tomas Hastrup <thastrup@gmail.com>
2024-10-10 09:42:42 -0300

    countdown

--- src/components/timer/timerClasses.ts
+++ src/components/timer/timerClasses.ts
@@ -1,11 +1,11 @@
-export class Timer {
-  private initialtime: number;
-  public constructor(initialTime: number) {
-    this.initialtime = initialTime;
-  }
-  start() {
-    // console.log("tomas");
-  }
-  stop() {}
-  reset() {}
-}
+// export class Timer {
+//   private initialtime: number;
+//   public constructor(initialTime: number) {
+//     this.initialtime = initialTime;
+//   }
+//   start() {
+//     // console.log("tomas");
+//   }
+//   stop() {}
+//   reset() {}
+// }
